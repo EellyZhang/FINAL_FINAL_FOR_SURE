3c59b8771fb08e0b0a537b694b9c9d5a9046a03e
EellyZhang <31869183+EellyZhang@users.noreply.github.com>
2017-12-13 17:33:17 -0500

    Add files via upload

---
+++ MainSceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainSceneController : MonoBehaviour {
+	public static float runningTheGame;
+	public static bool isRestarted;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void NextScene()
+	{
+		SceneManager.LoadScene("StartingScene");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
---
+++ StartingSceneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartingSceneController : MonoBehaviour {
+	public static float timeBeforeStart;
+	public static bool isStarted;
+
+
+
+	// Use this for initialization
+	void Start () {
+		timeBeforeStart = 0.0f;
+		isStarted = false;
+	}
+
+
+	public void NextScene()
+	{
+		SceneManager.LoadScene("MainScene");
+		isStarted = true;
+		GameController.eachGameTime = 60.0f + timeBeforeStart;
+		GameController.turnTimer = 2.0f + timeBeforeStart;
+
+//		print ("isStarted");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isStarted == false) {
+			timeBeforeStart = Time.time;
+		}
+
+	}
+}
